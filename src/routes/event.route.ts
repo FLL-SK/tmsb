8eecad95e9e76b0c949e3fe7138ee7d6eed94bac
Jozef Sovcik <jozef@sovcik.net>
2020-09-23 11:53:48 +0200

    generic post handler for updating all fields

--- src/routes/event.route.ts
+++ src/routes/event.route.ts
@@ -83,6 +83,56 @@ router.get('/:id', Auth.jwt(), async function (req: RequestEvent, res, next) {
     }
 });
 
+router.post('/:id/fields', Auth.jwt(), async function (req: RequestEvent, res, next) {
+    let debug = debugLib.extend('post/:id/fields');
+    debug('/event/:ID/fields - post');
+
+    getRoles(req, debug);
+
+    debug('Body %O', req.body);
+    debug('User %O', req.user);
+
+    // no modifications allowed unless user is program manager or admin
+    if (!req.user || (!req.user.isAdmin && !req.user.isEventManager)) {
+        return resErr(res, 401, 'permission denied');
+    }
+    if (!req.event) return resErr(res, 404, 'event not found');
+
+    try {
+        for (let k in req.body) {
+            // @ts-ignore - waiting for https://github.com/DefinitelyTyped/DefinitelyTyped/pull/48098
+            let t = Event.schema.path(k).instance;
+
+            switch (t) {
+                case 'Number':
+                case 'String':
+                case 'Boolean':
+                    req.event.set(k, req.body[k]);
+                    break;
+                case 'Array':
+                    if (req.body[k]['$push'])
+                        (req.event.get(k) as mongoose.Types.Array<any>).push(
+                            req.event.get(k + '.$push')
+                        );
+                    if (req.body[k]['$pull'])
+                        (req.event.get(k) as mongoose.Types.Array<any>).push(
+                            req.event.get(k + '.$push')
+                        );
+                default:
+                    throw new Error('Unhandled SchemaType=' + t);
+            }
+        }
+        debug('Changed %O', req.event);
+        await req.event.save();
+    } catch (err) {
+        logERR('POST fields err=%s', err.message);
+        return resErr(res, 500, 'error updating field');
+    }
+
+    await populateEvent(req);
+    res.json(req.event);
+});
+
 router.get('/', Auth.jwt(), async function (req: RequestEvent, res, next) {
     const debug = debugLib.extend('get/');
     const cmd = req.query.cmd;
