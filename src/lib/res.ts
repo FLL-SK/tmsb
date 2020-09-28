e7235b23e3de940e96f29d24784063e30268d5c0
Jozef Sovcik <jozef@sovcik.net>
2020-09-28 09:24:47 +0200

    resErr modified

--- src/lib/res.ts
+++ src/lib/res.ts
@@ -1,4 +1,4 @@
 import { Response } from 'express';
-export function resErr(res: Response, code = 500, message: string) {
-    return res.status(code).json({ error: message });
+export function resErr(res: Response, code = 500, message: string, resultCode?: string) {
+    return res.status(code).json({ code: resultCode, message: message });
 }
