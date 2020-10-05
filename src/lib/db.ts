06d9c85794549df6e087d53e6e6cfb2ffe6d9cea
Jozef Sovcik <jozef@sovcik.net>
2020-10-05 21:17:49 +0200

    fixing DB url

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,12 +6,12 @@ const logERR = require('debug')('ERROR:lib-db');
 const logWARN = require('debug')('WARN:lib-db');
 const logINFO = require('debug')('INFO:lib-db');
 
-const url =
-    'mongodb://' +
-    (process.env.DB_USER ? process.env.DB_USER + ':' + process.env.DB_PWD + '@' : '') +
-    process.env.DB_SERVER +
-    '/' +
-    process.env.DB_DATABASE;
+const url = (process.env.DB_URL || 'not-set')
+    .replace('$DB_USER', process.env.DB_USER ? process.env.DB_USER : '')
+    .replace(
+        ':$DB_PWD@',
+        process.env.DB_PWD ? ':' + process.env.DB_PWD + '@' : process.env.DB_USER ? '@' : ''
+    );
 
 export function connectDB() {
     return new Promise(async function (fulfill, reject) {
@@ -25,10 +25,9 @@ export function connectDB() {
 
             mongoose.connection.once('open', function () {
                 logINFO(
-                    'Connected successfully to database @' +
-                        process.env.DB_SERVER +
-                        '/' +
-                        process.env.DB_DATABASE
+                    'Connected successfully to %s as user %s',
+                    process.env.DB_URL,
+                    process.env.DB_USER
                 );
             });
 
